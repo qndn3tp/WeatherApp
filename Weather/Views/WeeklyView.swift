d10827a837a9562e398b17cd4017ed70e7af7e11
qndn3tp <qndn3tp@naver.com>
2025-09-02 14:51:37 +0900

    feat: Weekly 탭 뷰 추가

--- Weather/Views/WeeklyView.swift
+++ Weather/Views/WeeklyView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 struct WeeklyView: View {
     // MARK: - Body
     var body: some View {
-        Text("위치")
+        ScrollView {
+            // 상단
+            HStack(spacing: 42) {
+                // 왼쪽: 아바타 프로필
+                Image("avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70, height: 190)
+                
+                // 오른쪽: 이번달 weekly날씨 정보
+                Text("이번달 weekly날씨 정보")
+                    .frame(width: 187, height: 146)
+                    .background(.white)
+            }
+            VStack(alignment: .leading, spacing: 0) {
+                Text("주간 날씨")
+                    .font(.titleSmall)
+                Text("주간 날씨 정보")
+                    .frame(width: 359, height: 300)
+                    .background(.white)
+                Text("유용한 정보")
+                    .font(.titleSmall)
+                Text("날씨에 따른 생활정보, 코디 팁 등 큐레이션")
+                    .font(.system(size: 14))
+                    .frame(width: 359, height: 100)
+                    .background(.white)
+            }
+        }
+        .padding(.horizontal, 20)
     }
 }
 
